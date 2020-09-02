ae8babb6354f7718418672769c35e80cf279707b
alyona0909 <70573142+alyona0909@users.noreply.github.com>
2020-09-02 10:08:23 +0300

    Update JPGtoPNGconverter.py

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -10,15 +10,15 @@ def convert_to_png(first_folder, second_folder, name_file):
 
 # function to check is folder's name is valid
 def check_valid_input(name_folder):
-	valid_pattern = re.compile(r"[a-zA-Z]?[:]?[\/]?[a-zA-Z0-9\,\.\-\/]{1,}\/?")
+	valid_pattern = re.compile(r"[a-zA-Z]?[:]?[\/]?[a-zA-Z0-9\,\.\-\/_]{1,}\/?")
 	return valid_pattern.fullmatch(name_folder)
 
 # fuction to format input name folder
 def valid_name_folder(name_folder):
+	if "/" not in name_folder:
+		name_folder = "./" + name_folder
 	if name_folder[-1]!='/':
 		name_folder += '/'
-	if ":/" not in name_folder:
-		name_folder = "./" + name_folder
 	return name_folder
 
 def main_function(first_folder, second_folder):
@@ -28,6 +28,9 @@ def main_function(first_folder, second_folder):
 
 		first_folder = valid_name_folder(first_folder)
 		second_folder = valid_name_folder(second_folder)
+
+		if not os.path.exists(first_folder):
+			raise Exception("First folder does not exist")
 
 		# if second folder not exists - to create
 		if not os.access(second_folder, os.F_OK):
@@ -39,20 +42,26 @@ def main_function(first_folder, second_folder):
 		if not name_pics_list:
 			raise Exception("First folder must contain files")	
 
+		flag = 0
 		for name in name_pics_list:
-			if ".jpg" not in name:
+			if ".jpg" in name:
+				flag += 1
+			if not flag:
 				raise Exception("First folder must contain jpg files")
 
 		# loop through first folder and convert
 		for name_pic in name_pics_list:
 			convert_to_png(first_folder, second_folder, name_pic)
+		return 0
 	except Exception as err:
 		print(err)
+		return str(err)
 
-# grab the first and second argument
-# first - folder's name with jpg image
-# second - folder's name with png image
-first_folder = sys.argv[1]
-second_folder = sys.argv[2]
+if __name__ == '__main__':
+	# grab the first and second argument
+	# first - folder's name with jpg image
+	# second - folder's name with png image
+	first_folder = sys.argv[1]
+	second_folder = sys.argv[2]
 
-main_function(first_folder, second_folder)
+	main_function(first_folder, second_folder)
